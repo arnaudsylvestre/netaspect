ad76e172c8b49b0d750d87fe2d23a307f325bdc5
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2013-12-01 13:40:50 +0100

    Ajout de la tâche

--- src/FluentAspect.Weaver.Tasks/FluentAspectWeaveTask.cs
+++ src/FluentAspect.Weaver.Tasks/FluentAspectWeaveTask.cs
@@ -2,18 +2,23 @@
 using System.Diagnostics;
 using System.IO;
 using FluentAspect.Weaver.Factory;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks;
 using Microsoft.Build.Utilities;
 
 namespace FluentAspect.Weaver.Tasks
 {
     public class FluentAspectWeaveTask : Task
     {
+        [Required]
+        public string AssemblyFile { get; set; }
+
        public override bool Execute()
         {
             try
             {                
                 var weaverCore = WeaverCoreFactory.Create();
-
+                weaverCore.Weave(AssemblyFile);
             }
             catch (Exception e)
             {
