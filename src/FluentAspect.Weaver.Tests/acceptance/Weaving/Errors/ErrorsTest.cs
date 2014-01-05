b4d980654422aa1741be3f42da954f3c2d25e380
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-01-05 21:08:39 +0100

    Refactoring

--- src/FluentAspect.Weaver.Tests/acceptance/Weaving/Errors/ErrorsTest.cs
+++ src/FluentAspect.Weaver.Tests/acceptance/Weaving/Errors/ErrorsTest.cs
@@ -13,26 +13,26 @@ namespace FluentAspect.Weaver.Tests.acceptance.Weaving.Errors
 
             Assert.AreEqual(new List<string>
             {
-                "The aspect FluentAspect.Sample.AOP.CheckWithVoidInterceptorAttribute doesn't have a Before/After/OnException method",
-"The aspect FluentAspect.Sample.AOP.CheckNotRenameInAssemblyAttribute doesn't have a Before/After/OnException method",
-"The aspect FluentAspect.Sample.MethodWeaving.Problems.Warnings.EmptyAspectAttribute doesn't have a Before/After/OnException method",           
+"The aspect FluentAspect.Sample.MethodWeaving.Problems.Warnings.EmptyAspectAttribute doesn't have a Before/After/OnException method",
+"The aspect FluentAspect.Sample.AOP.CheckWithVoidInterceptorAttribute doesn't have a Before/After/OnException method",
+"The aspect FluentAspect.Sample.AOP.CheckNotRenameInAssemblyAttribute doesn't have a Before/After/OnException method",          
             }, errorHandler.Warnings);
             Assert.AreEqual(new List<string>
             {
             }, errorHandler.Failures);
             Assert.AreEqual(new List<string>
                 {
-                   "A method declared in interface can not be weaved : InterfaceToWeaveWithAttributes.CheckBeforeWithAttributes",
-"impossible to ref the parameter 'parameters' in the method After of the type 'FluentAspect.Sample.AOP.CheckWithParametersReferencedAttribute'",
-"An abstract method can not be weaved : AbstractClassToWeaveWithAttributes.CheckBeforeWithAttributes",
-"impossible to ref the parameter 'method' in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckMethodReferencedInAfterAspectAttribute'",
-"the method parameter in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckMethodWithWrongTypeInAfterAspectAttribute' is declared with the type 'System.Int32' but it is expected to be System.Reflection.MethodInfo",
-"impossible to ref the parameter 'parameters' in the method OnException of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckParametersReferencedOnExceptionAspectAttribute'",
-"impossible to ref the parameter 'parameters' in the method Before of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckParametersReferencedInBeforeAspectAttribute'",
-"impossible to ref the parameter 'parameters' in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckParametersReferencedInAfterAspectAttribute'",
+                   "impossible to ref the parameter 'parameters' in the method OnException of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckParametersReferencedOnExceptionAspectAttribute'",
+"the result parameter in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckResultInVoidAspectAttribute' is declared with the type 'System.Int32&' but it is expected to be System.Void because the return type of the method Check in the type FluentAspect.Sample.MethodWeaving.Parameters.ToCheckResultInVoid",
 "impossible to ref the parameter 'instance' in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckInstanceReferencedInAfterAspectAttribute'",
 "the instance parameter in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckInstanceWithWrongTypeInAfterAspectAttribute' is declared with the type 'System.Int32' but it is expected to be System.Object or FluentAspect.Sample.MethodWeaving.Parameters.ToCheckInstanceWithWrongTypeInAfter",
-"the result parameter in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckResultInVoidAspectAttribute' is declared with the type 'System.Int32&' but it is expected to be System.Void because the return type of the method Check in the type FluentAspect.Sample.MethodWeaving.Parameters.ToCheckResultInVoid",
+"impossible to ref the parameter 'parameters' in the method Before of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckParametersReferencedInBeforeAspectAttribute'",
+"the method parameter in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckMethodWithWrongTypeInAfterAspectAttribute' is declared with the type 'System.Int32' but it is expected to be System.Reflection.MethodInfo",
+"A method declared in interface can not be weaved : InterfaceToWeaveWithAttributes.CheckBeforeWithAttributes",
+"impossible to ref the parameter 'parameters' in the method After of the type 'FluentAspect.Sample.AOP.CheckWithParametersReferencedAttribute'",
+"impossible to ref the parameter 'parameters' in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckParametersReferencedInAfterAspectAttribute'",
+"An abstract method can not be weaved : AbstractClassToWeaveWithAttributes.CheckBeforeWithAttributes",
+"impossible to ref the parameter 'method' in the method After of the type 'FluentAspect.Sample.MethodWeaving.Parameters.ToCheckMethodReferencedInAfterAspectAttribute'",
                 }, errorHandler.Errors);
         }
 
