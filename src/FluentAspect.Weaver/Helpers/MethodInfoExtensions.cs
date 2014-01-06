fc08df191a8d7d0919052702cffd55020a2b143b
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-01-06 21:00:17 +0100

    Refactoring

--- src/FluentAspect.Weaver/Helpers/MethodInfoExtensions.cs
+++ src/FluentAspect.Weaver/Helpers/MethodInfoExtensions.cs
@@ -7,11 +7,23 @@ namespace FluentAspect.Weaver.Helpers
 {
     public static class MethodInfoExtensions
     {
-        public static List<NetAspectAttribute> GetNetAspectAttributes(this ICustomAttributeProvider method, bool inherit)
+        //public static List<NetAspectAttribute> GetNetAspectAttributes(this ICustomAttributeProvider method, bool inherit)
+        //{
+        //    IEnumerable<NetAspectAttribute> customAttributes_L =
+        //        method.GetCustomAttributes(inherit).Select((a) => new NetAspectAttribute(a));
+        //    return (from m in customAttributes_L where m.IsValid select m).ToList();
+        //}
+        public static List<MethodWeavingConfiguration> GetMethodWeavingAspectAttributes(this ICustomAttributeProvider method, bool inherit)
         {
             IEnumerable<NetAspectAttribute> customAttributes_L =
                 method.GetCustomAttributes(inherit).Select((a) => new NetAspectAttribute(a));
-            return (from m in customAttributes_L where m.IsValid select m).ToList();
+            return (from m in customAttributes_L where m.IsValid && m.Kind == NetAspectAttributeKind.MethodWeaving select m.MethodWeavingConfiguration).ToList();
+        }
+        public static List<CallWeavingConfiguration> GetCallWeavingAspectAttributes(this ICustomAttributeProvider method, bool inherit)
+        {
+            IEnumerable<NetAspectAttribute> customAttributes_L =
+                method.GetCustomAttributes(inherit).Select((a) => new NetAspectAttribute(a));
+            return (from m in customAttributes_L where m.IsValid && m.Kind == NetAspectAttributeKind.CallWeaving select m.CallWeavingConfiguration).ToList();
         }
     }
 }
