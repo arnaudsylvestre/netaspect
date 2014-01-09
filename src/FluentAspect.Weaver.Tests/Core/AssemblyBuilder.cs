e0e12847f620615529b1a51ac305b6e304ca1e7b
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-01-09 21:27:29 +0100

    Add framework to generate assemblies

--- src/FluentAspect.Weaver.Tests/Core/AssemblyBuilder.cs
+++ src/FluentAspect.Weaver.Tests/Core/AssemblyBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Mono.Cecil;
 
 namespace FluentAspect.Weaver.Tests.Core
@@ -15,9 +16,26 @@ namespace FluentAspect.Weaver.Tests.Core
         }
     }
 
+    public class TypeDefinitionDefiner
+    {
+        private TypeDefinition type;
+
+        public TypeDefinitionDefiner(TypeDefinition type)
+        {
+            this.type = type;
+        }
+
+        public MethodDefinitionDefiner WithMethod(string methodName)
+        {
+            var methodDefinition = new MethodDefinition(methodName, MethodAttributes.Public, type.Module.Import(typeof (void)));
+            type.Methods.Add(methodDefinition);
+            return new MethodDefinitionDefiner(methodDefinition);
+        }
+    }
+
     public class AssemblyDefinitionDefiner
     {
-        private const string DefaultNamespace = "A";
+        public const string DefaultNamespace = "A";
 
         private readonly AssemblyDefinition _assemblyDefinition;
 
@@ -26,16 +44,96 @@ namespace FluentAspect.Weaver.Tests.Core
             _assemblyDefinition = assemblyDefinition;
         }
 
-        public MethodDefinitionDefinber WithMethod(string typeName, string methodName)
+        public TypeDefinitionDefiner WithType(string typeName)
         {
-            var typeDefinition = _assemblyDefinition.MainModule.GetType(DefaultNamespace, typeName);
-            if (typeDefinition == null)
-                _assemblyDefinition.MainModule.Types.Add(new TypeDefinition(DefaultNamespace, ));
+            var typeDefinition = CreateType(typeName);
+            return new TypeDefinitionDefiner(typeDefinition);
         }
 
-        public CallWeavingAspectDefiner WithCallWeavingAspect()
+        private TypeDefinition CreateType(string typeName)
         {
-            
+            var typeDefinition = new TypeDefinition(DefaultNamespace, typeName, TypeAttributes.Class);
+            _assemblyDefinition.MainModule.Types.Add(typeDefinition);
+            return typeDefinition;
+        }
+
+        public CallWeavingAspectDefiner WithCallWeavingAspect(string typeName)
+        {
+            return new CallWeavingAspectDefiner(CreateType(typeName));
+        }
+
+        public MethodWeavingAspectDefiner WithMethodWeavingAspect(string typeName)
+        {
+            return new MethodWeavingAspectDefiner(CreateType(typeName));
+        }
+
+        public void Save(string filename)
+        {
+            _assemblyDefinition.Write(filename);
+        }
+    }
+
+    public class MethodWeavingAspectDefiner
+    {
+        private readonly TypeDefinition typeDefinition;
+
+        public MethodWeavingAspectDefiner(TypeDefinition typeDefinition)
+        {
+            this.typeDefinition = typeDefinition;
+            typeDefinition.BaseType = typeDefinition.Module.Import(typeof (Attribute));
+            typeDefinition.Methods.Add(new MethodDefinition(".ctor", MethodAttributes.Public, typeDefinition.Module.Import(typeof(void))));
+        }
+
+        public MethodReference Constructor
+        {
+            get { return (from m in typeDefinition.Methods where m.Name == ".ctor" select m).First(); }
+        }
+
+        public MethodDefinitionDefiner AddBefore()
+        {
+            var definition = new MethodDefinition("Before", MethodAttributes.Public, typeDefinition.Module.Import(typeof(void)));
+            typeDefinition.Methods.Add(definition);
+            return new MethodDefinitionDefiner(definition);
+        }
+    }
+
+    public class CallWeavingAspectDefiner
+    {
+        private TypeDefinition typeDefinition;
+
+        public CallWeavingAspectDefiner(TypeDefinition typeDefinition)
+        {
+            this.typeDefinition = typeDefinition;
+            typeDefinition.BaseType = typeDefinition.Module.Import(typeof(Attribute));
+        }
+
+        public MethodDefinitionDefiner AddBeforeCall()
+        {
+            var definition = new MethodDefinition("BeforeCall", MethodAttributes.Public, typeDefinition.Module.Import(typeof (object)));
+            typeDefinition.Methods.Add(definition);
+            return new MethodDefinitionDefiner(definition);
+        }
+    }
+
+    public class MethodDefinitionDefiner
+    {
+        private readonly MethodDefinition _definition;
+
+        public MethodDefinitionDefiner(MethodDefinition definition)
+        {
+            _definition = definition;
+        }
+
+        public MethodDefinitionDefiner WithParameter<T>(string name)
+        {
+            _definition.Parameters.Add(new ParameterDefinition(name, ParameterAttributes.None,
+                                                                   _definition.Module.Import(typeof (T))));
+            return this;
+        }
+
+        public void AddAspect(MethodWeavingAspectDefiner aspect)
+        {
+            _definition.CustomAttributes.Add(new CustomAttribute(aspect.Constructor));
         }
     }
 }
---
+++ src/FluentAspect.Weaver.Tests/unit/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAspect.Weaver.Tests.Core;
+using NUnit.Framework;
+
+namespace FluentAspect.Weaver.Tests.unit
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void Launch()
+        {
+            var assembly = AssemblyBuilder.Create();
+            var method = assembly.WithType("MyClassToWeave").WithMethod("MyMethodToWeave");
+            var aspect = assembly.WithMethodWeavingAspect("MyAspectAttribute");
+            aspect.AddBefore();
+            method.AddAspect(aspect);
+            //method.WhichContainsThrowException();
+            assembly.Save("TempAssembly.dll");
+
+
+        }
+    }
+}
