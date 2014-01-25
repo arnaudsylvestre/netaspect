f80ef699f421c6f2d5e06e51b0b316e4e1766683
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-01-25 13:29:51 +0100

    Add tests

--- src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Method/Parameters/After/AfterMethodParametersParameterTest.cs
+++ src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Method/Parameters/After/AfterMethodParametersParameterTest.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Reflection;
 using FluentAspect.Weaver.Tests.Core;
+using FluentAspect.Weaver.Tests.Core.Model;
 using NUnit.Framework;
 
 namespace FluentAspect.Weaver.Tests.unit.MethodWeaving.Method.Parameters.After
@@ -10,21 +11,72 @@ namespace FluentAspect.Weaver.Tests.unit.MethodWeaving.Method.Parameters.After
        [Test]
        public void CheckParametersReferenced()
         {
-            throw new NotImplementedException();
+            DoUnit2.Test().ByDefiningAssembly(assembly =>
+            {
+                var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+                var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+                aspect.AddAfterInterceptor()
+                   .WithReferencedParameter<object[]>("parameters")
+                   .WithReturn();
+                myClassToWeave.AddMethod("MyMethodToWeave")
+                   .WithReturn()
+                   .WithAspect(aspect);
+
+            })
+           .EnsureErrorHandler(errorHandler => errorHandler.Errors.Add(string.Format("impossible to ref/out the parameter 'parameters' in the method After of the type 'A.MyAspectAttribute'")))
+                   .AndLaunchTest();
           
        }
 
         [Test]
         public void CheckParametersBadType()
        {
-           throw new NotImplementedException();
+           DoUnit2.Test().ByDefiningAssembly(assembly =>
+           {
+               var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+               var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+               aspect.AddAfterInterceptor()
+                  .WithParameter<int>("parameters")
+                  .WithReturn();
+               myClassToWeave.AddMethod("MyMethodToWeave")
+                  .WithReturn()
+                  .WithAspect(aspect);
+
+           })
+           .EnsureErrorHandler(errorHandler => errorHandler.Errors.Add(string.Format("the parameters parameter in the method After of the type 'A.MyAspectAttribute' is declared with the type 'System.Int32' but it is expected to be System.Object[]")))
+                   .AndLaunchTest();
            
         }
 
        [Test]
        public void CheckParametersWithRealType()
         {
-            throw new NotImplementedException();
+            DoUnit2.Test().ByDefiningAssembly(assembly =>
+            {
+                var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+                var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+                var netAspectInterceptor = aspect.AddAfterInterceptor();
+                netAspectInterceptor
+                   .WithParameter<object[]>("parameters");
+                netAspectInterceptor
+                    .WithReturn();
+                myClassToWeave.AddMethod("MyMethodToWeave")
+                    .WithParameter<int>("first")
+                   .WithReturn()
+                   .WithAspect(aspect);
+
+            })
+                   .AndEnsureAssembly(assemblyP =>
+                   {
+                       var o = assemblyP.CreateObject("MyClassToWeave");
+                       o.CallMethod("MyMethodToWeave", 12);
+                       Assert.AreEqual(new object[]
+                           {
+                               12
+                           }, assemblyP.GetStaticFieldValue("MyAspectAttribute", "AfterparametersField"));
+
+                   })
+                   .AndLaunchTest();
           
        }
    }
---
+++ src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Method/Parameters/After/AfterMethodUnknownParameterTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using FluentAspect.Weaver.Tests.Core;
+using FluentAspect.Weaver.Tests.Core.Model;
+using NUnit.Framework;
+
+namespace FluentAspect.Weaver.Tests.unit.MethodWeaving.Method.Parameters.After
+{
+    [TestFixture]
+    public class AfterMethodUnknownParameterTest
+    {
+        [Test]
+         public void CheckUnknown()
+         {
+             DoUnit2.Test().ByDefiningAssembly(assembly =>
+             {
+                 var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+                 var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+                 aspect.AddAfterInterceptor()
+                    .WithParameter<int>("unknownParameter")
+                    .WithReturn();
+                 myClassToWeave.AddMethod("MyMethodToWeave")
+                    .WithReturn()
+                    .WithAspect(aspect);
+
+             })
+           .EnsureErrorHandler(errorHandler => errorHandler.Errors.Add(string.Format("The parameter unknownParameter is not supported")))
+                   .AndLaunchTest();
+         }
+
+        [Test]
+        public void CheckWithParameterNameSameAsKeyword()
+        {
+            DoUnit2.Test().ByDefiningAssembly(assembly =>
+            {
+                var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+                var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+                aspect.AddAfterInterceptor()
+                   .WithParameter("instance", myClassToWeave)
+                   .WithReturn();
+                myClassToWeave.AddMethod("MyMethodToWeave")
+                    .WithParameter<int>("instance")
+                   .WithReturn()
+                   .WithAspect(aspect);
+
+            })
+           .AndEnsureAssembly(assemblyP =>
+           {
+
+               var o = assemblyP.CreateObject("MyClassToWeave");
+               o.CallMethod("MyMethodToWeave");
+               Assert.AreEqual(o, assemblyP.GetStaticFieldValue("MyAspectAttribute", "AfterinstanceField"));
+
+           })
+                   .AndLaunchTest();
+        }
+    }
+}
