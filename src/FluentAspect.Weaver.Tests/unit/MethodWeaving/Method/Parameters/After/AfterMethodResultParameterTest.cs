90cf42be524905851f6ec129b51a85b1e4899344
Arnaud SYLVESTRE <Arnaud.Sylvestre@acs-inc.com>
2014-02-06 12:55:38 +0100

    Ajout de tests

--- src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Method/Parameters/After/AfterMethodResultParameterTest.cs
+++ src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Method/Parameters/After/AfterMethodResultParameterTest.cs
@@ -14,28 +14,141 @@ namespace FluentAspect.Weaver.Tests.unit.MethodWeaving.Method.Parameters.After
       [Test]
       public void CheckResultReferenced()
       {
-         throw new NotImplementedException();
+         DoUnit2.Test().ByDefiningAssembly(assembly =>
+         {
+            var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+            var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+            var netAspectInterceptor = aspect.AddAfterInterceptor();
+            netAspectInterceptor
+               .WithReferencedParameter<string>("result");
+            netAspectInterceptor
+               .WithUpdateParameter("result", "New Hello")
+                .WithReturn();
+            myClassToWeave.AddMethod<string>("MyMethodToWeave")
+               .WithReturn("Hello")
+               .WithAspect(aspect);
+
+         })
+                   .AndEnsureAssembly(assemblyP =>
+                   {
+
+                      var o = assemblyP.CreateObject("MyClassToWeave");
+                      var res = o.CallMethod("MyMethodToWeave");
+                      Assert.AreEqual("Hello", assemblyP.GetStaticFieldValue("MyAspectAttribute", "AfterresultField"));
+                      Assert.AreEqual("New Hello", res);
+
+                   })
+                   .AndLaunchTest();
       }
       [Test]
       public void CheckResultReferencedWithObjectType()
       {
-         throw new NotImplementedException();
+         DoUnit2.Test().ByDefiningAssembly(assembly =>
+         {
+            var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+            var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+            var netAspectInterceptor = aspect.AddAfterInterceptor();
+            netAspectInterceptor
+               .WithReferencedParameter<object>("result");
+            netAspectInterceptor
+                .WithReturn();
+            myClassToWeave.AddMethod<string>("MyMethodToWeave")
+               .WithReturn("Hello")
+               .WithAspect(aspect);
+
+         })
+                   .AndEnsureAssembly(assemblyP =>
+                   {
+
+                      var o = assemblyP.CreateObject("MyClassToWeave");
+                      o.CallMethod("MyMethodToWeave");
+                      Assert.AreEqual("Hello", assemblyP.GetStaticFieldValue("MyAspectAttribute", "AfterresultField"));
+
+                   })
+                   .AndLaunchTest();
       }
       [Test]
       public void CheckResult()
       {
-         throw new NotImplementedException();
+         DoUnit2.Test().ByDefiningAssembly(assembly =>
+           {
+               var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+               var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+               var netAspectInterceptor = aspect.AddAfterInterceptor();
+               netAspectInterceptor
+                  .WithParameter<string>("result");
+               netAspectInterceptor
+                   .WithReturn();
+               myClassToWeave.AddMethod<string>("MyMethodToWeave")
+                  .WithReturn("Hello")
+                  .WithAspect(aspect);
+
+           })
+                   .AndEnsureAssembly(assemblyP =>
+                   {
+
+                       var o = assemblyP.CreateObject("MyClassToWeave");
+                       o.CallMethod("MyMethodToWeave");
+                       Assert.AreEqual("Hello", assemblyP.GetStaticFieldValue("MyAspectAttribute", "AfterresultField"));
+
+                   })
+                   .AndLaunchTest();
+        
       }
       [Test]
       public void CheckResultWithObjectType()
       {
-         throw new NotImplementedException();
+         DoUnit2.Test().ByDefiningAssembly(assembly =>
+         {
+            var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+            var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+            var netAspectInterceptor = aspect.AddAfterInterceptor();
+            netAspectInterceptor
+               .WithParameter<object>("result");
+            netAspectInterceptor
+                .WithReturn();
+            myClassToWeave.AddMethod<string>("MyMethodToWeave")
+               .WithReturn("Hello")
+               .WithAspect(aspect);
+
+         })
+                   .AndEnsureAssembly(assemblyP =>
+                   {
+
+                      var o = assemblyP.CreateObject("MyClassToWeave");
+                      o.CallMethod("MyMethodToWeave");
+                      Assert.AreEqual("Hello", assemblyP.GetStaticFieldValue("MyAspectAttribute", "AfterresultField"));
+
+                   })
+                   .AndLaunchTest();
       }
 
       [Test]
       public void CheckResultWhenVoidMethod()
       {
-         throw new NotImplementedException();
+         DoUnit2.Test().ByDefiningAssembly(assembly =>
+         {
+            var myClassToWeave = assembly.AddClass("MyClassToWeave").WithDefaultConstructor();
+            var aspect = assembly.AddDefaultAspect("MyAspectAttribute");
+            var netAspectInterceptor = aspect.AddAfterInterceptor();
+            netAspectInterceptor
+               .WithParameter<object>("result");
+            netAspectInterceptor
+                .WithReturn();
+            myClassToWeave.AddMethod("MyMethodToWeave")
+               .WithReturn()
+               .WithAspect(aspect);
+
+         })
+                   .AndEnsureAssembly(assemblyP =>
+                   {
+
+                      var o = assemblyP.CreateObject("MyClassToWeave");
+                      o.CallMethod("MyMethodToWeave");
+                      Assert.AreEqual("Hello", assemblyP.GetStaticFieldValue("MyAspectAttribute", "AfterresultField"));
+
+                   })
+                   .AndLaunchTest();
       }
        
    }
