d4f56ffbd53aff9910f4b89c1cd694e4887ae513
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-03-02 11:26:25 +0100

    Add tests

---
+++ src/FluentAspect.Weaver.Tests/unit/CallWeaving/Fields/Getter/Parameters/After/Caller/AfterMethodInstanceParameterWithRealTypeTest.cs
@@ -0,0 +1,45 @@
+using System;
+using NUnit.Framework;
+
+namespace FluentAspect.Weaver.Tests.unit.MethodWeaving.Method.Parameters.After.Instance
+{
+    public class AfterCallGetFieldCallerParameterWithRealTypeTest : NetAspectTest<AfterCallGetFieldCallerParameterWithRealTypeTest.ClassToWeave>
+   {
+      protected override Action CreateEnsure()
+      {
+         return () =>
+            {
+               Assert.IsNull(MyAspect.Caller);
+               var classToWeave_L = new ClassToWeave();
+               classToWeave_L.Weaved();
+               Assert.AreEqual(classToWeave_L, MyAspect.Caller);
+            };
+      }
+
+      public class ClassToWeave
+      {
+
+          [MyAspect]
+          public string Field;
+
+         public string Weaved()
+         {
+             return Field;
+         }
+      }
+
+      public class MyAspect : Attribute
+      {
+         public bool NetAspectAttribute = true;
+
+         public static ClassToWeave Caller;
+
+         public void AfterGetField(ClassToWeave caller)
+         {
+             Caller = caller;
+         }
+      }
+   }
+
+   
+}
