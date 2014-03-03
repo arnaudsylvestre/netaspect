cd1229e66ce301c1c8ddf53ecf4c1e3d0b184bf7
Arnaud SYLVESTRE <Arnaud.Sylvestre@acs-inc.com>
2014-03-03 13:25:42 +0100

    Add tests

---
+++ src/FluentAspect.Weaver.Tests/unit/CallWeaving/Fields/Getter/Parameters/Before/CalledParameterName/BeforeCallGetFieldCalledParameterNameParameterWithObjectTypeTest.cs
@@ -0,0 +1,45 @@
+using System;
+using NUnit.Framework;
+
+namespace FluentAspect.Weaver.Tests.unit.CallWeaving.Fields.Getter.Parameters.Before.CalledParameterName
+{
+    public class BeforeCallGetFieldCalledParameterNameParameterWithObjectTypeTest : NetAspectTest<BeforeCallGetFieldCalledParameterNameParameterWithObjectTypeTest.ClassToWeave>
+   {
+      protected override Action CreateEnsure()
+      {
+         return () =>
+            {
+               Assert.AreEqual(0, MyAspect.ParameterName);
+               var classToWeave_L = new ClassToWeave();
+               classToWeave_L.Weaved(12);
+               Assert.AreEqual(12, MyAspect.ParameterName);
+            };
+      }
+
+      public class ClassToWeave
+      {
+
+          [MyAspect]
+          public string Field;
+
+         public string Weaved(int param1)
+         {
+             return Field;
+         }
+      }
+
+      public class MyAspect : Attribute
+      {
+         public bool NetAspectAttribute = true;
+
+         public static object ParameterName;
+
+         public void BeforeGetField(object calledParam1)
+         {
+             ParameterName = calledParam1;
+         }
+      }
+   }
+
+   
+}
---
+++ src/FluentAspect.Weaver.Tests/unit/CallWeaving/Fields/Getter/Parameters/Before/FileName/BeforeCallGetFieldFileNameParameterWithRealTypeTest.cs
@@ -0,0 +1,45 @@
+using System;
+using NUnit.Framework;
+
+namespace FluentAspect.Weaver.Tests.unit.CallWeaving.Fields.Getter.Parameters.Before.FileName
+{
+    public class BeforeCallGetFieldFileNameParameterWithRealTypeTest : NetAspectTest<BeforeCallGetFieldFileNameParameterWithRealTypeTest.ClassToWeave>
+   {
+      protected override Action CreateEnsure()
+      {
+         return () =>
+            {
+               Assert.AreEqual(null, MyAspect.FileName);
+               var classToWeave_L = new ClassToWeave();
+               classToWeave_L.Weaved();
+               Assert.AreEqual("", MyAspect.FileName);
+            };
+      }
+
+      public class ClassToWeave
+      {
+
+          [MyAspect]
+          public string Field;
+
+         public string Weaved()
+         {
+             return Field;
+         }
+      }
+
+      public class MyAspect : Attribute
+      {
+         public bool NetAspectAttribute = true;
+
+         public static string FileName;
+
+         public void BeforeGetField(string fileName)
+         {
+             FileName = fileName;
+         }
+      }
+   }
+
+   
+}
