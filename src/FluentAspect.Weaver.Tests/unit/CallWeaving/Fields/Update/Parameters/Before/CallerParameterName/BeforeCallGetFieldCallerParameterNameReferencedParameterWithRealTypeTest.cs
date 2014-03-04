f0870b0845ef3d9d6a911643c8ce0c17f06e9930
Arnaud SYLVESTRE <Arnaud.Sylvestre@acs-inc.com>
2014-03-04 09:08:18 +0100

    Add tests

---
+++ src/FluentAspect.Weaver.Tests/unit/CallWeaving/Fields/Update/Parameters/Before/CallerParameterName/BeforeCallGetFieldCallerParameterNameReferencedParameterWithRealTypeTest.cs
@@ -0,0 +1,47 @@
+using System;
+using NUnit.Framework;
+
+namespace FluentAspect.Weaver.Tests.unit.CallWeaving.Fields.Getter.Parameters.Before.ParameterName
+{
+    public class BeforeCallUpdateFieldCallerParameterNameReferencedParameterWithRealTypeTest : NetAspectTest<BeforeCallUpdateFieldCallerParameterNameReferencedParameterWithRealTypeTest.ClassToWeave>
+    {
+        protected override Action CreateEnsure()
+        {
+            return () =>
+            {
+                Assert.AreEqual(0, MyAspect.ParameterName);
+                var classToWeave_L = new ClassToWeave();
+                int val = 12;
+                classToWeave_L.Weaved(ref val);
+                Assert.AreEqual(25, val);
+                Assert.AreEqual(12, MyAspect.ParameterName);
+            };
+        }
+
+        public class ClassToWeave
+        {
+
+            [MyAspect]
+            public string Field;
+
+            public void Weaved(ref int param1)
+            {
+                Field = "Hello";
+            }
+        }
+
+        public class MyAspect : Attribute
+        {
+            public bool NetAspectAttribute = true;
+
+            public static int ParameterName;
+
+            public void BeforeUpdateField(int callerParam1)
+            {
+                ParameterName = callerParam1;
+            }
+        }
+    }
+
+
+}
