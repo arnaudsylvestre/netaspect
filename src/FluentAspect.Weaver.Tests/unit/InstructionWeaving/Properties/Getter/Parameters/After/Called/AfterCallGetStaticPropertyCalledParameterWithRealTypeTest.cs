1bce8be024beff0a1b7f732091c3dc47975dc7db
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-05-14 19:29:46 +0200

    Correct the tests

--- src/FluentAspect.Weaver.Tests/unit/InstructionWeaving/Properties/Getter/Parameters/After/Called/AfterCallGetStaticPropertyCalledParameterWithRealTypeTest.cs
+++ src/FluentAspect.Weaver.Tests/unit/InstructionWeaving/Properties/Getter/Parameters/After/Called/AfterCallGetStaticPropertyCalledParameterWithRealTypeTest.cs
@@ -18,7 +18,7 @@ namespace NetAspect.Weaver.Tests.unit.InstructionWeaving.Properties.Getter.Param
                     Level = ErrorLevel.Warning,
                     Message =
                     string.Format(
-                        "the called parameter in the method AfterGetProperty of the type '{0}' is not available for static property : default value will be passed",
+                        "the called parameter in the method AfterGetProperty of the type '{0}' is not available for static member : default value will be passed",
                         typeof(MyAspect).FullName)
                 });
         }
