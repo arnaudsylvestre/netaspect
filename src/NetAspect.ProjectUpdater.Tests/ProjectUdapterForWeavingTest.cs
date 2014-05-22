616b4c8849a3a50322bf488dad8f248f764d260f
Arnaud SYLVESTRE <Arnaud.Sylvestre@acs-inc.com>
2014-05-22 17:04:27 +0200

    Start creation of exe file to update the csproj file

---
+++ src/NetAspect.ProjectUpdater.Tests/ProjectUdapterForWeavingTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NetAspect.ProjectUpdater.Tests
+{
+   [TestFixture]
+   public class ProjectUdapterForWeavingTest
+   {
+      [Test]
+      public void CheckUpdateProject()
+      {
+         ProjectUdapterForWeaving projectUdapter_L = new ProjectUdapterForWeaving();
+         projectUdapter_L.Load("Sample.csproj");
+         projectUdapter_L.AddNetAspectSupport();
+         projectUdapter_L.Save("SampleUpdated.csproj");
+         FileAssert.AreEqual("Expected.csproj", "SampleUpdated.csproj");
+      }
+   }
+
+   public class ProjectUdapterForWeaving
+   {
+      public void Load(string pathToFile)
+      {
+         
+      }
+   }
+}
---
+++ src/NetAspect.ProjectUpdater/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NetAspect.ProjectUpdater
+{
+   static class Program
+   {
+      /// <summary>
+      /// Point d'entrée principal de l'application.
+      /// </summary>
+      [STAThread]
+      static void Main(string[] args)
+      {
+         List<string> projectFilesToWeave = new List<string>();
+         if (args.Length < 1)
+         OpenFileDialog openFileDialog = new OpenFileDialog();
+         if (openFileDialog.ShowDialog() == DialogResult.OK)
+         {
+            
+         }
+      }
+   }
+}
