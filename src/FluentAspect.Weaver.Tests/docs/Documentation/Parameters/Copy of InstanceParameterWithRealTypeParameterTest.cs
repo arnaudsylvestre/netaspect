257a67bf95ed4365c68e578819b90f568267fc04
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-08-28 23:58:04 +0200

    Refactoring website

---
+++ src/FluentAspect.Weaver.Tests/docs/Documentation/Parameters/Copy of InstanceParameterWithRealTypeParameterTest.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Reflection;
+using NetAspect.Weaver.Tests.unit;
+using NUnit.Framework;
+
+namespace NetAspect.Weaver.Tests.docs.Documentation.Parameters
+{
+   public class MethodParameterWithRealTypeTest : NetAspectTest<MethodParameterWithRealTypeTest.MyInt>
+    {
+       public MethodParameterWithRealTypeTest()
+           : base("It must be of System.Reflection.MethodBase type", "MethodWeavingBefore", "MethodWeaving")
+      {
+      }
+
+
+      public class MyInt
+        {
+            int value;
+
+            public MyInt(int value)
+            {
+                this.value = value;
+            }
+
+            public int Value
+            {
+                get { return value; }
+            }
+            [Log]
+            public int DivideBy(int v)
+            {
+                return value / v;
+            }
+        }
+
+        protected override Action CreateEnsure()
+        {
+            return () =>
+                {
+                    var myInt = new MyInt(24);
+                    Assert.AreEqual(2, myInt.DivideBy(12));
+                    Assert.AreEqual(myInt.GetType().GetMethod("DivideBy"), LogAttribute.MethodInfo);
+                };
+        }
+
+
+
+        public class LogAttribute : Attribute
+        {
+           public bool NetAspectAttribute = true;
+           public static MethodBase MethodInfo;
+
+           public void Before(MethodBase method)
+           {
+               MethodInfo = method;
+           }
+        }
+    }
+}
