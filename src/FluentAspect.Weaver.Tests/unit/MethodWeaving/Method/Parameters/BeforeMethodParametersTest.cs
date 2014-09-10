e72b1c4017f21ab9d51be9428d36a585fbfd07fa
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-09-10 17:40:15 +0200

    Refactoring

---
+++ src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Method/Parameters/BeforeMethodParametersTest.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace NetAspect.Weaver.Tests.unit.MethodWeaving.Method.Parameters
+{
+   public class BeforeMethodParametersTest : NetAspectTest<BeforeMethodParametersTest.MyInt>
+   {
+      public BeforeMethodParametersTest()
+         : base("This method is executed before the code of the method is executed", "MethodWeavingBefore", "MethodWeaving")
+      {
+      }
+
+
+      public class MyInt
+      {
+         private readonly int value;
+
+         public MyInt(int value)
+         {
+            this.value = value;
+         }
+
+         public int Value
+         {
+            get { return value; }
+         }
+
+         [Log]
+         public int DivideBy(int v)
+         {
+            return value / v;
+         }
+      }
+
+      protected override Action CreateEnsure()
+      {
+         return () =>
+         {
+            var myInt = new MyInt(24);
+            Assert.AreEqual(2, myInt.DivideBy(12));
+            Assert.True(LogAttribute.Called);
+         };
+      }
+
+
+      public class LogAttribute : Attribute
+      {
+         public static bool Called;
+         public bool NetAspectAttribute = true;
+
+         public void BeforeMethod(object instance, MethodBase method, object[] parameters, int v)
+         {
+            Called = true;
+            Assert.AreEqual(typeof (MyInt), instance.GetType());
+            Assert.AreEqual("DivideBy", method.Name);
+            Assert.AreEqual(1, parameters.Length);
+            Assert.AreEqual(12, v);
+         }
+      }
+   }
+}
---
+++ src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Method/Parameters/OnFinallyMethodParametersTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace NetAspect.Weaver.Tests.unit.MethodWeaving.Method.Parameters
+{
+   public class OnFinallyMethodParametersTest : NetAspectTest<OnFinallyMethodParametersTest.MyInt>
+   {
+      public OnFinallyMethodParametersTest()
+         : base("This method is executed after the method is executed if an exception is raised or not", "MethodWeavingOnFinally", "MethodWeaving")
+      {
+      }
+
+      public class MyInt
+      {
+         private readonly int value;
+
+         public MyInt(int value)
+         {
+            this.value = value;
+         }
+
+         public int Value
+         {
+            get { return value; }
+         }
+
+         [Log]
+         public int DivideBy(int v)
+         {
+            return value / v;
+         }
+      }
+
+      protected override Action CreateEnsure()
+      {
+         return () =>
+         {
+            var myInt = new MyInt(24);
+            myInt.DivideBy(12);
+            Assert.True(LogAttribute.Called);
+         };
+      }
+
+
+      public class LogAttribute : Attribute
+      {
+         public static bool Called;
+         public bool NetAspectAttribute = true;
+
+         public void OnFinallyMethod(object instance, MethodBase method, object[] parameters, int v)
+         {
+            Called = true;
+            Assert.AreEqual(typeof (MyInt), instance.GetType());
+            Assert.AreEqual("DivideBy", method.Name);
+            Assert.AreEqual(1, parameters.Length);
+            Assert.AreEqual(12, v);
+         }
+      }
+   }
+}
