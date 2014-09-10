6e9418e2ff0c36b7c22a4446988cd09ae73a4bf8
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-09-10 06:30:42 +0200

    Refactoring

---
+++ src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Properties/Updater/Parameters/AfterPropertySetMethodParametersTest.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace NetAspect.Weaver.Tests.unit.MethodWeaving.Properties.Updater.Parameters
+{
+   public class AfterPropertySetMethodParametersTest : NetAspectTest<AfterPropertySetMethodParametersTest.MyInt>
+   {
+      public AfterPropertySetMethodParametersTest()
+         : base("After Property Setter Weaving possibilities", "PropertySetWeavingAfter", "PropertySetWeaving")
+      {
+      }
+
+      public class MyInt
+      {
+         private int value;
+
+         public MyInt(int value)
+         {
+            this.value = value;
+         }
+
+         [Log]
+         public int Value
+         {
+            set { this.value = value; }
+         }
+
+         public int DivideBy(int v)
+         {
+            return value / v;
+         }
+      }
+
+      protected override Action CreateEnsure()
+      {
+         return () =>
+         {
+            var myInt = new MyInt(24);
+            myInt.Value = 32;
+            Assert.True(LogAttribute.Called);
+         };
+      }
+
+
+      public class LogAttribute : Attribute
+      {
+         public static bool Called;
+         public bool NetAspectAttribute = true;
+
+         public void AfterPropertySetMethod(object instance, PropertyInfo property, int value)
+         {
+            Called = true;
+            Assert.AreEqual(typeof (MyInt), instance.GetType());
+            Assert.AreEqual("Value", property.Name);
+            Assert.AreEqual(32, value);
+         }
+      }
+   }
+}
---
+++ src/FluentAspect.Weaver.Tests/unit/MethodWeaving/Properties/Updater/Parameters/OnFinallyPropertySetMethodParametersTest.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace NetAspect.Weaver.Tests.unit.MethodWeaving.Properties.Updater.Parameters
+{
+   public class OnFinallyPropertySetMethodParametersTest : NetAspectTest<OnFinallyPropertySetMethodParametersTest.MyInt>
+   {
+      public OnFinallyPropertySetMethodParametersTest()
+         : base("On finally property setter weaving possibilities", "PropertySetWeavingOnFinally", "PropertySetWeaving")
+      {
+      }
+
+      public class MyInt
+      {
+         private int value;
+
+         public MyInt(int value)
+         {
+            this.value = value;
+         }
+
+         [Log]
+         public int Value
+         {
+            set { this.value = value; }
+         }
+
+         public int DivideBy(int v)
+         {
+            return value / v;
+         }
+      }
+
+      protected override Action CreateEnsure()
+      {
+         return () =>
+         {
+            var myInt = new MyInt(24);
+            myInt.Value = 12;
+            Assert.True(LogAttribute.Called);
+         };
+      }
+
+
+      public class LogAttribute : Attribute
+      {
+         public static bool Called;
+         public bool NetAspectAttribute = true;
+
+         public void OnFinallyPropertySetMethod(object instance, PropertyInfo property, int value)
+         {
+            Called = true;
+            Assert.AreEqual(typeof (MyInt), instance.GetType());
+            Assert.AreEqual("Value", property.Name);
+            Assert.AreEqual(12, value);
+         }
+      }
+   }
+}
