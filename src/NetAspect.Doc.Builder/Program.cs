08e38ace76f9ca80d90f7b295b8920e2ec688b36
Arnaud.Sylvestre <Arnaud.Sylvestre@gmail.com>
2014-10-27 21:30:09 +0100

    Remove DocBuilder tests

--- src/NetAspect.Doc.Builder/Program.cs
+++ src/NetAspect.Doc.Builder/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace NetAspect.Doc.Builder
+﻿using System.IO;
+using NetAspect.Doc.Builder.Core;
+using NetAspect.Doc.Builder.Model;
+
+namespace NetAspect.Doc.Builder
 {
    internal class Program
    {
       private static void Main(string[] args)
       {
+          string tests = args[1];
+          if (!Directory.Exists(tests))
+              Directory.CreateDirectory(tests);
+
+          var generator = new WebsiteGenerator();
+          WebSite webSite = WebsiteFactory.Create(args[0]);
+          generator.Generate(webSite, tests);
       }
    }
 }
